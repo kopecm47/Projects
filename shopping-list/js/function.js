43a7e28af98073a995adbb3a2a3fa343e6d6610e
kopecm47 <kopecm47@gmail.com>
2015-08-07 03:48:32 -0400

    initial commit

--- shopping-list/js/function.js
+++ shopping-list/js/function.js
@@ -1,27 +1,61 @@
 $(document).ready(function () {
 
+//define global variables
+	var checkMark = '<span class="cross-off"></span>'
+	var deleteMark = '<span class="delete"></span>'
+
 //add items using enter
-	$(#item).keyup(function(event){
-		if(event.keyCode == 13) {
-			event.preventDefault()
-			$("#add").click();
-		};
-	});
-//add items using + button
-	$("#add").click(function(){
-		var txtbox = document.getElementById("item");
-		var txtvar = txtbox.value;
-		event.preventDefault();
+	function addItem(){
+		$("#item").keyup(function(event){
+			if(event.keyCode == 13) {
+				postItem();
+			}
+		});
+		$(document).on("click", "#add", function(){
+			postItem();	
+		});		
+	}
 
-		if (!.trim($("item").val())) {
-			alert("Please enter text");
-		} else {
-			$("<li class="items"></li>").appendTo("#list").html("<div class="box"></div><span>" + txt val + "</span>");
-			document.getElementById("item").value = "";
-		};
-	});
-//delete list items
-//cross off list items and add check mark
-//sortable list items
+	addItem();
 
+//posting the item
+function postItem(){
+
+	var item = $("#item").val();
+	var work = '<p class="full-width no-strike">' + checkMark + item + deleteMark + '</p>';
+	$("#list").prepend(work);
+	$('#item').val('');
+}
+
+//cross items off
+function crossOff(){
+	$('.cross-off').toggle(function(){
+		$(this).closest('p').addClass("strike");
+	}, function(){
+		$(this).closest('p').removeClass("active");
+	});
+}
+
+//clear list
+	$(document).on("click", "#reset", function(){
+		$("#list").empty();
+	});
+
+//delete list item
+	$(document).on("click", ".delete", function(){
+		$(this).closest('p').remove();
+	});
+//check off list items
+	$(document).on("click", ".cross-off", function(){
+	    if (!$(this).closest('p').hasClass('strike')) {
+	        $(this).closest('p').addClass("strike");
+	        $(this).siblings('.delete').addClass("alt-delete");
+	        $(this).addClass("alt-cross-off");
+	    }
+	    else {
+	        $(this).closest('p').removeClass("strike");
+	        $(this).siblings('.delete').removeClass("alt-delete");
+	        $(this).removeClass("alt-cross-off");
+   		}
+	});
 });
